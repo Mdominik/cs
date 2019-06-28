ef439c266a8863de088a676fb8972ee0af42156b
Mdominik <mazniakd@gmail.com>
2019-06-28 10:20:49 -0700

    Bank, Accounts classes

---
+++ 2_Beginner-Intermediate C++/Assignments/6 - OOP Bank/build/CMakeFiles/bank.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bank.dir/src/main.cpp.o"
+  "../bin/bank.pdb"
+  "../bin/bank"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
